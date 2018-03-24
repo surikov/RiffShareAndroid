00cc166a511d60734995b0e6e62530421eb5307e
Srgy Surkv <sss1024@gmail.com>
2018-03-24 16:24:48 +0300

    test

--- app/src/main/assets/RiffShareFlat.js
+++ app/src/main/assets/RiffShareFlat.js
@@ -20,7 +20,7 @@ RiffShareFlat.prototype.init = function () {
 	this.onAir = false;
 	this.queueAhead = 0.75;
 	this.tickerDelay = 1;
-	this.tickerStep=0;
+	this.tickerStep = 0;
 	console.log('queueAhead', this.queueAhead);
 	this.svgns = "http://www.w3.org/2000/svg";
 	this.contentDiv = document.getElementById('contentDiv');
@@ -306,7 +306,7 @@ RiffShareFlat.prototype.init = function () {
 	var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
 	this.audioContext = new AudioContextFunc();
 	this.player = new WebAudioFontPlayer();
-	this.player.afterTime=0.1;
+	this.player.afterTime = 0.1;
 	this.master = new WebAudioFontChannel(this.audioContext);
 	this.echoOn = false;
 	try {
@@ -316,10 +316,10 @@ RiffShareFlat.prototype.init = function () {
 			 || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i//
 			.test(usrAgnt.substr(0, 4))) {
 			this.echoOn = false;
-			document.title='Mobile RiffShare';
+			document.title = 'Mobile RiffShare';
 		} else {
 			this.echoOn = true;
-			document.title='Desktop RiffShare';
+			document.title = 'Desktop RiffShare';
 		}
 	} catch (noch) {
 		console.log(noch);
@@ -934,13 +934,13 @@ RiffShareFlat.prototype.queueNextBeats = function () {
 		}
 		//console.log('	envelopes', this.player.envelopes.length);
 		var wait = 0.5 * 1000 * (this.nextWhen - this.audioContext.currentTime);
-		if(this.echoOn){
+		//if (this.echoOn) {
 			this.tickerStep++;
-			if(this.tickerStep>=this.tickerDelay){
+			if (this.tickerStep >= this.tickerDelay) {
 				this.moveBeatCounter();
-				this.tickerStep=0;
-			}			
-		}
+				this.tickerStep = 0;
+			}
+		//}
 		this.tickID = setTimeout(function () {
 				riffshareflat.queueNextBeats();
 			}, wait);
@@ -1032,7 +1032,7 @@ RiffShareFlat.prototype.cauntToneMeasures = function (nn) {
 	return le;
 }
 RiffShareFlat.prototype.sendNextBeats = function (when, startBeat, endBeat) {
-	console.log('sendNextBeats',when, startBeat, endBeat,this.player.envelopes.length);
+	console.log('sendNextBeats', when, startBeat, endBeat, this.player.envelopes.length);
 	this.sentWhen = when;
 	this.sentBeat = startBeat;
 	var N = 4 * 60 / this.tempo;
@@ -1490,7 +1490,7 @@ RiffShareFlat.prototype.tileCounter = function (left, top, width, height) {
 			this.tileRectangle(g, 0, 0, this.tapSize * 0.001, this.tapSize * 0.001, '#000');
 			this.tileRectangle(g, this.innerWidth, this.innerHeight, this.tapSize * 0.001, this.tapSize * 0.001, '#000');
 
-			this.counterLine = this.tileRectangle(g, x + this.tapSize * 0.3, y, this.tapSize *0.4, h, this.findTrackInfo(0).shadow);
+			this.counterLine = this.tileRectangle(g, x + this.tapSize * 0.3, y, this.tapSize * 0.4, h, this.findTrackInfo(0).shadow);
 		}
 	}
 };
@@ -1858,8 +1858,7 @@ RiffShareFlat.prototype.collision = function (x1, y1, w1, h1, x2, y2, w2, h2) {
 		 || x1 > x2 + w2 //
 		 || y1 + h1 < y2 //
 		 || y1 > y2 + h2 //
-	)
-	{
+	) {
 		return false;
 	} else {
 		return true;
